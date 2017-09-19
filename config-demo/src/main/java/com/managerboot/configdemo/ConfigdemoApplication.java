3bfa3796670025fbef9e645584ffb4a6683e3276
huabo <huabo1900@163.com>
2017-09-19 14:48:24 +0800

    配置中心使用示例

---
+++ config-demo/src/main/java/com/managerboot/configdemo/ConfigdemoApplication.java
@@ -0,0 +1,14 @@
+package com.managerboot.configdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConfigdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigdemoApplication.class, args);
+	}
+}
---
+++ config-demo/src/main/java/com/managerboot/configdemo/bean/ConfigBean.java
@@ -0,0 +1,7 @@
+package com.managerboot.configdemo.bean;
+
+/**
+ * Created by huabo on 2017/9/18.
+ */
+public class ConfigBean {
+}
